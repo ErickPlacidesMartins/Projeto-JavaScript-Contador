961da804a4bf7bcbbc8aa8c0fbf87709921912c7
Erick Placides <103293578+ErickPlacidesMartins@users.noreply.github.com>
2023-01-23 19:10:51 -0300

    Atualização Projeto

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,13 +1,50 @@
-var currentNumberWrapper = document.getElementById("currentNumber")
-var currentNumber = 0;
+let hour = document.getElementById('hour');
+let minute = document.getElementById('minute');
+let secund = document.getElementById('secund');
 
-function increment(){
-    currentNumber = currentNumber + 1;
-    currentNumberWrapper.innerHTML = currentNumber;
+const start = document.getElementById('start');
+const pause = document.getElementById('pause');
+const stop = document.getElementById('stop');
+
+function display(){
+    if(hour.value == 0 && minute.value == 0 && secund.value == 0){
+        hour.value = 0;
+        minute.value = 0;
+        secund.value = 0;
+        document.getElementById('sound').play();
+        clearInterval(startDisplay);
+    } else if( secund.value != 0){
+        secund.value--;
+    } else if( minute.value != 0 && secund.value == 0){
+        secund.value = 59;
+        minute.value--;
+    } else if(hour.value != 0 && minute.value == 0){
+        minute.value = 60;
+        hour.value--;
+    }
+
+    return;
 }
 
-function decrement(){
-    currentNumber = currentNumber - 1;
-    currentNumberWrapper.innerHTML = currentNumber;
-    
-}
+
+start.addEventListener('click', function(){
+    function startInterval(){
+        startDisplay = setInterval(function() {
+            display();
+        }, 1000);       
+    }
+    startInterval()
+})
+
+pause.addEventListener('click', function(){
+    clearInterval(startDisplay)
+})
+
+stop.addEventListener('click', function(){
+    hour.value = 0;
+    minute.value = 0;
+    secund.value = 0;
+    clearInterval(startDisplay)
+
+})
+
